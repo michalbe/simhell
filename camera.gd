3143645272a7c7b43ef17ceb5ace632e840af6b2
Michał Budzyński <michal@virtualdesign.pl>
2018-02-25 22:17:27 +0100

    :city_sunset: gimball to camera rename script

--- camera.gd
+++ camera.gd
@@ -22,33 +22,32 @@ func _process(delta):
 		$inner_gimball.rotate_x(-camera_speed * delta)
 	if (Input.is_action_pressed("ui_down") and $inner_gimball.rotation.x < downmost_gimball_position):
 		$inner_gimball.rotate_x(camera_speed * delta)
-	
+
 	print($inner_gimball.rotation)
-	
+
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.is_pressed() and not event.is_echo():
 	#		Zoom OUT
 			if event.button_index and event.button_index == BUTTON_WHEEL_UP and get_scale().x < max_zoom:
 				set_scale(get_scale() + Vector3(zoom_speed, zoom_speed, zoom_speed))
-			
+
 	#		Zoom IN
 			if event.button_index == BUTTON_WHEEL_DOWN and get_scale().x > min_zoom:
 				set_scale(get_scale() - Vector3(zoom_speed, zoom_speed, zoom_speed))
-				
+
 		if event.button_index == BUTTON_RIGHT:
 			is_dragged = event.is_pressed()
-			
+
 #	elif event is InputEventMouseMotion:
 #	TODO: IMPLEMENT DRAGGING HERE
 #		print("Mouse Motion at: ", event.position, is_dragged)
 #		move_local_x(event.relative_x)
 #		move_local_y(event.relative_y)
-	
+
 func _ready():
 	rotate_y(default_camera_y)
 	$inner_gimball.rotate_x(default_camera_x)
-	
+
 	set_process_input(true)
 	set_process(true)
-	
