a59035ad15ac1fac62b23c68adeddb8694d2023d
Michał Budzyński <michal@virtualdesign.pl>
2018-02-25 22:32:00 +0100

    :cupid: random tile generationfor the floor

--- Game.gd
+++ Game.gd
@@ -8,10 +8,10 @@ export var size = 2
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	
+	var tiles = $GridMap.theme.get_item_list().size()
 	for x in range (size):
 		for y in range (size):
-			$GridMap.set_cell_item(x - size/2, 0, y - size/2, 0)
+			$GridMap.set_cell_item(x - size/2, 0, y - size/2, 0) #randi() % tiles)
 	pass
 
 #func _process(delta):
