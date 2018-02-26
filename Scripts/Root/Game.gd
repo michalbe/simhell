c3e2417fd055faf62447ce2a5011c6047f9dd96e
Michał Budzyński <michal@virtualdesign.pl>
2018-02-26 16:44:12 +0100

    :snowboarder: selector visible only in building state

--- Scripts/Root/Game.gd
+++ Scripts/Root/Game.gd
@@ -25,17 +25,20 @@ func _input(event):
 		mouse_pos = event.position
 
 func _physics_process(delta):
-	var camera = $CameraGimball/inner_gimball/Camera
-	var ray_origin = camera.project_ray_origin(mouse_pos)
-	var ray_direction = camera.project_ray_normal(mouse_pos)
-	
-	var from = ray_origin
-	var to = ray_origin + ray_direction * 1000
-	
-	var space_state = get_world().get_direct_space_state()
-	var hit = space_state.intersect_ray(from, to)
-	if hit.size() != 0:
-		$Selector.show()
-		$Selector.set_translation(Vector3(int(hit.position.x/cell_size) * cell_size, 0.45, int(hit.position.z/cell_size) * cell_size))
+	if $StatePanel.get_state() == $StatePanel.STATES.BUILDING:
+		var camera = $CameraGimball/inner_gimball/Camera
+		var ray_origin = camera.project_ray_origin(mouse_pos)
+		var ray_direction = camera.project_ray_normal(mouse_pos)
+		
+		var from = ray_origin
+		var to = ray_origin + ray_direction * 1000
+		
+		var space_state = get_world().get_direct_space_state()
+		var hit = space_state.intersect_ray(from, to)
+		if hit.size() != 0:
+			$Selector.show()
+			$Selector.set_translation(Vector3(int(hit.position.x/cell_size) * cell_size, 0.45, int(hit.position.z/cell_size) * cell_size))
+		else:
+			$Selector.hide()
 	else:
-		$Selector.hide()
+		$Selector.hide()	
