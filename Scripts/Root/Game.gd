979063184e68fa6e314448ef54f3685d34f73b6a
Michał Budzyński <michal@virtualdesign.pl>
2018-02-26 16:21:44 +0100

    :ear: proper raycasting

--- Scripts/Root/Game.gd
+++ Scripts/Root/Game.gd
@@ -1,27 +1,39 @@
 extends Spatial
 
 export var size = 2
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+
+var mouse_pos_x = 0
+var mouse_pos_y = 0
+var mouse_pos = Vector2(0, 0)
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	var tiles = $GridMap.theme.get_item_list().size()
 	for x in range (size):
 		for y in range (size):
 			$GridMap.set_cell_item(x - size/2, 0, y - size/2, 0) #randi() % tiles)
 	pass
 
+#func _input(event):
+#	if event is InputEventMouseMotion:
+##		$Selector.show()
+#		pass
+##		$selector.translate
+#	pass
+#
 func _input(event):
 	if event is InputEventMouseMotion:
-#		$Selector.show()
-		pass
-#		$selector.translate
-	pass
-			
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+		mouse_pos = event.position
+
+func _physics_process(delta):
+	var camera = $CameraGimball/inner_gimball/Camera
+	var ray_origin = camera.project_ray_origin(mouse_pos)
+	var ray_direction = camera.project_ray_normal(mouse_pos)
+	
+	var from = ray_origin
+	var to = ray_origin + ray_direction * 1000
+	
+	var space_state = get_world().get_direct_space_state()
+	var hit = space_state.intersect_ray(from, to)
+	print(hit)
+	if hit.size() != 0:
+		print(hit)
